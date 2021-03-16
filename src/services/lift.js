bb3f394d8321ea4e5b0a2b75ebe8c3c9ba4b8ef2
Styrle <jserpis@hotmail.co.uk>
2021-03-16 18:08:58 +0000

    Services

--- src/services/lift.js
+++ src/services/lift.js
@@ -4,105 +4,98 @@ import session from "./session";
 import moment from "moment";
 
 export default class {
-
-    static getByUserId = async userId => {
-        let result = {
-            data: null,
-            error: null
+  static getByUserId = async userId => {
+    let result = {
+      data: null,
+      error: null
         };
 
-        await axios.post(`${config.api}/lifts/all`, {userId: userId})
-            .then(resp => {
-                if (resp.status === 200) {
-                    result.data = resp.data;
-                }
-            })
-            .catch((err) => {
-                result.error = err.response.data;
-            });
+    await axios.post(`${config.api}/lifts/all`, {userId: userId})
+      .then(resp => {
+        if (resp.status === 200) {
+          result.data = resp.data;
+          }
+        })
+        .catch((err) => {
+          result.error = err.response.data;
+      });
+    return result;
+  }
 
-        return result;
+  static add = async json => {
+    let result = {
+      data: null,
+      error: null
+    };
+
+    const d = {
+      lifts: json,
+      userId: session.get('user')._id,
+      datetime: moment().format(),
     }
 
-    static add = async json => {
-        let result = {
-            data: null,
-            error: null
-        };
+    await axios.post(`${config.api}/lifts`, d)
+      .then((resp) => {
+        if (resp.status === 200) {
+          result.data = resp.data;
+          }
+        })
+        .catch((err) => {
+          result.error = err.response.data;
+        });
+      return result;
+    }
 
-        const d = {
-            lifts: json,
-            userId: session.get('user')._id,
-            datetime: moment().format(),
+  static getLatestByUserId = async userId => {
+    let result = {
+      data: null,
+      error: null
+    };
+    await axios.post(`${config.api}/lifts/latest`, {userId: userId})
+      .then((resp) => {
+        if (resp.status === 200) {
+          result.data = resp.data;
         }
-
-        await axios.post(`${config.api}/lifts`, d)
-            .then((resp) => {
-                if (resp.status === 200) {
-                    result.data = resp.data;
-                }
-            })
-            .catch((err) => {
-                result.error = err.response.data;
-            });
-
-        return result;
-    }
-
-    static getLatestByUserId = async userId => {
-        let result = {
-            data: null,
-            error: null
-        };
-
-        await axios.post(`${config.api}/lifts/latest`, {userId: userId})
-            .then((resp) => {
-                if (resp.status === 200) {
-                    result.data = resp.data;
-                }
-            })
-            .catch((err) => {
-                result.error = err.response.data;
-            });
-
-        return result;
+      })
+      .catch((err) => {
+        result.error = err.response.data;
+      });
+      return result;
     }
 
     static getLatestTwoByUserId = async userId => {
-        let result = {
-            data: null,
-            error: null
-        };
+      let result = {
+        data: null,
+        error: null
+      };
 
-        await axios.post(`${config.api}/lifts/latest-two`, {userId: userId})
-            .then((resp) => {
-                if (resp.status === 200) {
-                    result.data = resp.data;
-                }
-            })
-            .catch((err) => {
-                result.error = err.response.data;
-            });
-
-        return result;
+      await axios.post(`${config.api}/lifts/latest-two`, {userId: userId})
+        .then((resp) => {
+          if (resp.status === 200) {
+            result.data = resp.data;
+          }
+        })
+        .catch((err) => {
+          result.error = err.response.data;
+        });
+      return result;
     }
 
     static getAllByUserId = async userId => {
-        let result = {
-            data: null,
-            error: null
-        };
+      let result = {
+        data: null,
+        error: null
+      };
 
-        await axios.post(`${config.api}/lifts/all2`, {userId: userId})
-            .then((resp) => {
-                if (resp.status === 200) {
-                    result.data = resp.data;
-                }
-            })
-            .catch((err) => {
-                result.error = err.response.data;
-            });
-
-        return result;
+      await axios.post(`${config.api}/lifts/all2`, {userId: userId})
+        .then((resp) => {
+          if (resp.status === 200) {
+            result.data = resp.data;
+          }
+        })
+        .catch((err) => {
+          result.error = err.response.data;
+        });
+      return result;
     }
-}
+  }
--- src/services/user.js
+++ src/services/user.js
@@ -3,55 +3,53 @@ import config from '../config.json';
 //Handle our user requests
 export default class {
   //Handle login input
-    static login = async ({ email, password }) => {
-        let result = {
-            data: null,
-            error: null
-        };
+  static login = async ({ email, password }) => {
+    let result = {
+      data: null,
+      error: null
+    };
 
-        const data = {
-            email: email,
-            password: password
-        };
+    const data = {
+      email: email,
+      password: password
+    };
 
-        await axios.post(`${config.api}/users/login`, data)
-            .then((resp) => {
-                if (resp.status === 200) {
-                    result.data = resp.data;
-                }
-            })
-            .catch((err) => {
-                result.error = err.response.data;
-            });
-
-        return result;
+    await axios.post(`${config.api}/users/login`, data)
+      .then((resp) => {
+        if (resp.status === 200) {
+          result.data = resp.data;
+        }
+      })
+      .catch((err) => {
+        result.error = err.response.data;
+      });
+      return result;
     }
 
     //Handle singup input
     static signup = async ({ name, email, password, age, weight }) => {
-        let result = {
-            data: null,
-            error: null
-        };
+      let result = {
+        data: null,
+        error: null
+    };
 
-        const data = {
-            name: name,
-            email: email,
-            password: password,
-            age: age,
-            weight: weight
-        };
+    const data = {
+      name: name,
+      email: email,
+      password: password,
+      age: age,
+      weight: weight
+    };
 
-        await axios.post(`${config.api}/users/signup`, data)
-            .then((resp) => {
-                if (resp.status === 200) {
-                    result.data = resp.data;
-                }
-            })
-            .catch((err) => {
-                result.error = err.response.data;
-            });
-
-        return result;
-    }
+    await axios.post(`${config.api}/users/signup`, data)
+      .then((resp) => {
+        if (resp.status === 200) {
+          result.data = resp.data;
+        }
+      })
+      .catch((err) => {
+        result.error = err.response.data;
+      });
+    return result;
+  }
 }
